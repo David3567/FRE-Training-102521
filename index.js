3abcb81feb4133c9f75cff3088bd26bf8e23f7b6
szhao23 <stevenzhao456@gmai.com>
2021-10-27 01:53:29 -0500

    Finished Sign Up HTML Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+// // console.log("Hello World!");
+
+// console.log(a);
+
+// let a = 0;
+const h1ele = document.getElementsByTagName("h1");
+console.log(h1ele);
+h1ele[0].innerHTML = "Welcome to Antra";
