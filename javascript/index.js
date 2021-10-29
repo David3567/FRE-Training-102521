66628d5df7fa6153c6ed5152181ab7eec517033e
Zhengxu Wang <zhengxuwang@Zhengxus-MacBook-Pro.local>
2021-10-28 22:53:03 -0700

    update hw2

---
+++ javascript/index.js
@@ -0,0 +1,24 @@
+//ECMAscript: syntax ES6 
+//Javascript =ECMAScript + Web.api
+//Nodejs = ECMAscript + Node.api
+// console.log(window);
+
+//Primitive Data
+//string number booean undefined null symbol
+// var sb1 = Symbol('a');
+// var sb2 = Symbol('a');
+// console.log(sb1 == sb2);
+var str = 'abc';
+console.log(typeof str);
+var copystr = str;
+console.log(typeof copystr);
+
+console.log('string: ')
+
+//object Data
+var arr = [];
+var obj = {
+    name: 'Dio',
+    age: 200
+};
+var copyobj = obj;
