a63484f54635e77203aae6b6f223347929282ed9
szhao23 <stevenzhao456@gmai.com>
2021-10-31 22:46:28 -0500

    Finished Q3

--- homework/hw3_JavaScriptQ/index.js
+++ homework/hw3_JavaScriptQ/index.js
@@ -25,3 +25,23 @@ function Q2(input) {
 
 console.log(Q2("madam")); // true because "madam" is a palindrome
 console.log(Q2("apple")); // false because "apple" is not a palindrome
+
+// 3. Write a JavaScript function that generates all combinations of a string.
+// Example string: 'dog'
+// Expected Output: d, do, dog, o, og, g
+function Q3(input) {
+  stringList = new Array();
+  for (i = 0; i < input.length; i++) {
+    for (j = i + 1; j < input.length + 1; j++) {
+      stringList.push(input.slice(i, j));
+    }
+  }
+  return stringList;
+}
+
+console.log(Q3("dog"));
+
+// 4. Write a JavaScript function that returns a passed string with letters in alphabetical order.
+// Example string: 'webmaster'
+// Expected Output: 'abeemrstw'
+// Assume punctuation and numbers symbols are not included in the passed string.
