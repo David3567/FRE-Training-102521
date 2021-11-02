7c5696513d35a9e62ec1cc4006eedda363e3e7af
Fangsheng Chao <fchao002@ucr.edu>
2021-11-01 22:49:45 -0700

    function practice

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
-let a = "test"
-console.log(a);
+Array.prototype.myReduce = function (...args) {
+   let [acc, index] = args.length === 1
+      ? [this[0], 1]
+      : [args[1], 0];
+
+   for (let i = index; i < this.length; i++) {
+      acc = args[0](acc, this[i], i, this);
+   }
+   return acc;
+}
+const target = (a, b) => console.log(a + b);
+const fn = limitedFn(1, target);
+function limitedFn(num, cb) {
+   console.log('fn called');
+   let counter = num;
+   return (...rest) => {
+
+      if (counter > 0) {
+         counter--;
+         return cb(...rest);
+      }
+      else {
+         return console.log('over limit');
+      }
+   }
+}
+fn(1, 2);
+fn(2, 3);
+
