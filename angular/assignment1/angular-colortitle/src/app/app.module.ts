3f3972c2bcd53cad5fac765d10c9f7e752be1670
lukeyzhong <lukeyzhong@gmail.com>
2021-11-19 21:19:03 -0600

    update3

--- angular/assignment1/angular-colortitle/src/app/app.module.ts
+++ angular/assignment1/angular-colortitle/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { News1Component } from './components/news1/news1.component';
+import { News2Component } from './components/news2/news2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    News1Component
+    News1Component,
+    News2Component
   ],
   imports: [
     BrowserModule
--- angular/assignment1/angular-colortitle/src/app/components/news2/news2.component.ts
+++ angular/assignment1/angular-colortitle/src/app/components/news2/news2.component.ts
@@ -1,17 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-news1',
-  templateUrl: './news1.component.html',
-  styleUrls: ['./news1.component.css']
+  selector: 'app-news2',
+  templateUrl: './news2.component.html',
+  styleUrls: ['./news2.component.css']
 })
-export class News1Component implements OnInit {
+export class News2Component implements OnInit {
   heads: string = '“It’s like the elephant in the room,” Doctors urge caution as COVID-19 cases rise in Oklahoma again';
 
   constructor() { }
 
   ngOnInit(): void {
-    
   }
 
 }
