6e477c29ba395cd7b26fbbd50af02d6768d63167
lukeyzhong <lukeyzhong@gmail.com>
2021-11-19 22:18:42 -0600

    update4

--- angular/assignment1/angular-colortitle/src/app/components/news1/news1.component.ts
+++ angular/assignment1/angular-colortitle/src/app/components/news1/news1.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter} from '@angular/core';
+// import { EventEmitter } from 'stream';
 
 @Component({
   selector: 'app-news1',
@@ -6,12 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./news1.component.css']
 })
 export class News1Component implements OnInit {
-  heads: string = '“It’s like the elephant in the room,” Doctors urge caution as COVID-19 cases rise in Oklahoma again';
+  heads: string = 'Doctors urge caution as COVID-19 cases rise in Oklahoma again';
+  @Output() btnClick = new EventEmitter();
 
   constructor() { }
 
-  ngOnInit(): void {
-    
-  }
+  ngOnInit(): void { }
+    onClick() {
+      this.btnClick.emit();
+    }
+
 
 }
