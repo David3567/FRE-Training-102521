28294832aef6a3c864c08774e6bdb69a8d83ee78
AprilYXXiao <44380395+AprilYXXiao@users.noreply.github.com>
2021-11-21 17:29:23 -0800

    Update index.js

--- countDownClock/index.js
+++ countDownClock/index.js
@@ -1,1 +1,56 @@
 
+var start = document.getElementById('start');
+var reset = document.getElementById('reset');
+
+var h = document.getElementById("hour");
+var m = document.getElementById("minute");
+var s = document.getElementById("sec");
+
+//store a reference to the startTimer variable
+var startTimer = null;
+
+
+
+start.addEventListener('click', function(){
+    //initialize the variable
+    function startInterval(){
+        // The setInterval() method calls a function or evaluates an expression at specified intervals (in milliseconds).
+        startTimer = setInterval(function() {
+            timer();
+        }, 1000);
+    }
+    startInterval();
+})
+
+
+reset.addEventListener('click', function(){
+    h.value = 0;
+    m.value = 0;
+    s.value = 0;
+    //stop the timer after pressing "reset"
+    stopInterval()
+})
+
+function timer(){
+    if(h.value == 0 && m.value == 0 && s.value == 0){
+        h.value = 0;
+        m.value = 0;
+        s.value = 0;
+    } else if(s.value != 0){
+        s.value--;
+    } else if(m.value != 0 && s.value == 0){
+        s.value = 59;
+        m.value--;
+    } else if(h.value != 0 && m.value == 0){
+        m.value = 60;
+        h.value--;
+    }
+    return;
+}
+
+//stop the function after pressing the reset button, 
+//so the time wont go down when selecting a new time after pressing reset
+function stopInterval() {
+    //The clearInterval() method clears a timer set with the setInterval() method.
+    clearInterval(startTimer);
+}
