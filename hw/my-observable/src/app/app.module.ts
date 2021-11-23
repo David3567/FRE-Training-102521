4337c09ef2c2c4b769020b0510c1a8753091cfdb
Zhengqi Liu <44713737+tb869534072@users.noreply.github.com>
2021-11-23 15:32:09 -0800

    update observable

---
+++ hw/my-observable/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ObservableComponent } from './components/observable/observable.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ObservableComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
